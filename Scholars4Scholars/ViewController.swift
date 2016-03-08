78bf28061461f3caeaef0f766c86369f8c7c9c6f
Gaurav Lahiry <glahiry@andrew.cmu.edu>
2016-03-08 13:58:31 -0500

    initial xcode

---
+++ Scholars4Scholars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scholars4Scholars
+//
+//  Created by Gaurav Lahiry on 3/8/16.
+//  Copyright © 2016 Gaurav Lahiry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
